fd301de08f57aa88451174b5d8cf9233f3142b91
FateevMaxim <fateev.maxim@gmail.com>
2023-03-28 16:23:57 +0600

    delete track

--- resources/views/components/delete-track.blade.php
+++ resources/views/components/delete-track.blade.php
@@ -7,7 +7,7 @@
 
 <!-- Main modal -->
 <div id="defaultModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
-    <div class="relative w-2/4 h-full max-w-md md:h-auto">
+    <div class="relative w-2/4 max-w-md md:h-auto">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow">
             <!-- Modal header -->
